a4669abd38e80577c2d1600981d8d57cf5f682a1
Segun Olaiya <vadeshayo@gmail.com>
2025-01-30 19:34:22 +0100

    Add Confirm logic

---
+++ app/Actions/ConfirmSubscriptionAction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Actions;
+
+use App\Enums\SubscriptionStatusEnum;
+use App\Models\Subscription;
+
+class ConfirmSubscriptionAction
+{
+	public function __construct(private readonly string $token)
+	{
+
+	}
+
+	public function handle()
+	{
+		$subscription = Subscription::whereToken($this->token)->firstOrFail();
+		$subscription->update(['subscribed_at' => now(), 'status' => SubscriptionStatusEnum::SUBSCRIBED]);
+	}
+}
