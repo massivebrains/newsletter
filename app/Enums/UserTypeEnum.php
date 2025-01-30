3efd8c5da7fd2221548f9ffaedea002aae55a41d
Segun Olaiya <vadeshayo@gmail.com>
2025-01-30 18:16:36 +0100

    Added Users resource

--- app/Enums/UserTypeEnum.php
+++ app/Enums/UserTypeEnum.php
@@ -6,4 +6,11 @@ enum UserTypeEnum: string
 {
     case USER = 'user';
     case ADMIN = 'admin';
+
+    public static function getAsOptions(): array
+    {
+        return collect(self::cases())
+            ->mapWithKeys(fn(UserTypeEnum $code) => [$code->value => ucfirst($code->name)])
+            ->toArray();
+    }
 }
